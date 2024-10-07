2c4cbb7e477b7ac6d8c11c42b55ea5b5a0e986f8
Farmix2003 <ffarrux386@gmail.com>
2024-10-07 21:26:02 +0300

    Creation of user controller, service, implementation, and repository

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,10 +17,18 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        //        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        return null;
     }
 }
---
+++ src/main/java/com/example/springsecurity/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.springsecurity.service.serviceImpl;
+
+import com.example.springsecurity.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
